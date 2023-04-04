5f142c63fbaf88a3ee6bd98d1c104e204c46e390
AliArman01 <arman.ali@technomark.io>
2023-04-04 10:17:59 +0530

    OTP Api Working

--- contorllers/sms-controller.js
+++ contorllers/sms-controller.js
@@ -1,66 +1,29 @@
-const Sms = require('../models/sms')
-
-const twilio = require('twilio');
-
-
 const Nexmo = require('nexmo');
-
-const accountSid = 'AC0f95118b254ed5b9e56f36466964ce1e';
-const authToken = 'f791e936f53659296030e56cc85d1e5a';
-const twilioClient = twilio(accountSid, authToken);
-
-
-const generateRandomSixDigitsNumber = () => {
-    const min = 100000;
-    const max = 999999;
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
 
 exports.getMessege = (req, res, next) => {
     res.render('sms-page');
 }
 
 exports.postMessege = async (req, res, next) => {
-    try{
-    // const recipientPhoneNumber = req.body.phoneNumber;
-    // const randomNumber = generateRandomSixDigitsNumber();
-    // console.log("hii",recipientPhoneNumber)
-    // const message = `Hello from Arman ! Your verification code is: ${randomNumber}`;
-    // console.log("messege", message)
-    // const response = await twilioClient.messages.create({
-    //     from: +13602275105,
-    //     to: recipientPhoneNumber,
-    //     body: message,
-    // });
-    // return res.json({ message: `Message sent with id: ${response.sid}` });  
-        // client.messages
-        // .create({ body: "Hello from Twilio",
-        // from: +13602275105, 
-        // to: +917319977276
-        // })
-        // .then(message => console.log(message.sid));
-
-
+    try{   
+       const Mobileno = req.body.phoneNumber
+       console.log("Mobile no of reciever",Mobileno)
         const nexmo = new Nexmo({
-          apiKey: 'a3c30cd3',
-          apiSecret: 'cMq6CR5R0AtexPrM'
+          apiKey: '1b53634a',
+          apiSecret: 'OBo3WSPd0DxqvllP',
         });
+        const from = '+919265216270';
+        const to = Mobileno;
         const otp = Math.floor(1000 + Math.random() * 9000); // Generate a 4-digit OTP
-        const mobileNumber = '+917319977276'; // Replace with the actual mobile number
-        console.log("your otp is here",otp)
-        nexmo.message.sendSms(
-          '+918090562652', // Replace with your virtual number
-          mobileNumber,
-          `Hello Sams This is Your OTP  ${otp}`,
-          { type: 'unicode' },
-          (err, responseData) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.dir(responseData);
-            }
+        console.log("your Otp is",otp);
+        nexmo.message.sendSms(from, to, `Here is Your OTP for Verification by Arman Ali ${otp}`, (err, responseData) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(responseData);
           }
-        );     
+        });
+        res.send('otp send successfully')
 
   }
   catch (error) {
@@ -70,17 +33,5 @@ exports.postMessege = async (req, res, next) => {
 }
 
 
-exports.PostVerify = async (req, res) => {
-    const recipientPhoneNumber = req.body.phoneNumber;
-    const smsCodeReceived = req.body.smsCode;
 
-    const value = await redisClient.get(recipientPhoneNumber);
 
-    if (value === `${smsCodeReceived}`) {
-        await redisClient.del(recipientPhoneNumber);
-
-        return res.json({ message: 'This is a valid match!' });
-    }
-
-    return res.status(400).json({ message: `The phone number and the SMS code doesn't match.` });
-};
--- routes/sms-route.js
+++ routes/sms-route.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const userController = require('../contorllers/sms-controller');
 
 router.get('/',userController.getMessege);
-router.post('/sms',userController.postMessege);
 
-router.post('/verify-code',userController.PostVerify)
+router.post('/send-otp',userController.postMessege);
+
 
 module.exports = router
